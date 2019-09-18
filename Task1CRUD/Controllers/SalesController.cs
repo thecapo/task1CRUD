169e699c6d90bedf0ec4bf4180062d7f5efb82ef
thecapo <the7thcapo@gmail.com>
2019-09-18 22:27:56 +1000

    added CRUD for Sales

---
+++ Task1CRUD/Controllers/SalesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task1CRUD.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Task1CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        private readonly Onboarding_Assignment_Task1Context _context;
+
+        public SalesController(Onboarding_Assignment_Task1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sales
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sales>>> GetSales()
+        {
+            return await _context.Sales.ToListAsync();
+        }
+
+        // GET: api/Sales/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sales>> GetSale(int id)
+        {
+            var sales = await _context.Sales.FindAsync(id);
+
+            if (sales == null)
+            {
+                return NotFound();
+            }
+
+            return sales;
+        }
+
+        // POST: api/Sales
+        [HttpPost]
+        public async Task<ActionResult<Sales>> PostSales(Sales sales)
+        {
+            _context.Sales.Add(sales);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSale), new { id = sales.Id }, sales);
+        }
+
+        // PUT: api/Sales/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSales(int id, Sales sales)
+        {
+            if (id != sales.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sales).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Sales/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSales(int id)
+        {
+            var sales = await _context.Sales.FindAsync(id);
+
+            if (sales == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sales.Remove(sales);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
