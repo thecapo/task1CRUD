3dbb435f88da67076b3512dd7658a1ca6660f2c1
thecapo <the7thcapo@gmail.com>
2019-09-18 20:28:39 +1000

    added CRUD for Stores

---
+++ Task1CRUD/Controllers/StoresController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task1CRUD.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Task1CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoresController : ControllerBase
+    {
+        private readonly Onboarding_Assignment_Task1Context _context;
+
+        public StoresController(Onboarding_Assignment_Task1Context context)
+        {
+            _context = context;
+
+            if (_context.Store.Count() == 0)
+            {
+                // Create a new Store if collection is empty,
+                // which means you can't delete all Store.
+                _context.Store.Add(new Store { Name = "Store", Address = "Melbourne 3000" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Stores
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Store>>> GetStores()
+        {
+            return await _context.Store.ToListAsync();
+        }
+
+        // GET: api/Stores/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Store>> GetStore(int id)
+        {
+            var store = await _context.Store.FindAsync(id);
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return store;
+        }
+
+        // POST: api/Stores
+        [HttpPost]
+        public async Task<ActionResult<Store>> PostStore(Store store)
+        {
+            _context.Store.Add(store);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStore), new { id = store.Id }, store);
+        }
+
+        // PUT: api/Store/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStore(int id, Store store)
+        {
+            if (id != store.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(store).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Store/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStore(int id)
+        {
+            var store = await _context.Store.FindAsync(id);
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            _context.Store.Remove(store);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
