b773a7a2352926c9f1138a5043c7319ed6de7ef4
thecapo <the7thcapo@gmail.com>
2019-09-19 18:27:16 +1000

    added data annotations for customer, product and store

--- Task1CRUD/Models/Customer.cs
+++ Task1CRUD/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Task1CRUD.Models
 {
@@ -11,7 +12,13 @@ namespace Task1CRUD.Models
         }
 
         public int Id { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
+        [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
 
         public virtual ICollection<Sales> Sales { get; set; }
--- Task1CRUD/Models/Product.cs
+++ Task1CRUD/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Task1CRUD.Models
 {
@@ -11,7 +12,13 @@ namespace Task1CRUD.Models
         }
 
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 2)]
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(1, 9999, ErrorMessage = "Salary must be between 1 and 9999")]
         public int Price { get; set; }
 
         public virtual ICollection<Sales> Sales { get; set; }
